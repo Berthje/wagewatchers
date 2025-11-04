542de2738fa1b442c1e470dba5f4d9350accaba1
Layton Berth <laytonbrth@gmail.com>
2025-11-04 17:44:55 +0100

    refactor: remove unused location and report types from models index

--- src/types/models/index.ts
+++ src/types/models/index.ts
@@ -3,7 +3,4 @@
  * Centralized export for all domain models
  */
 
-export * from './salary.types';
 export * from './comment.types';
-export * from './report.types';
-export * from './location.types';
